c52b79ae68bb05888308f0a9c83a11274aed03ac
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-09-02 17:59:25 -0400

    modal partially complete

--- src/ModalPopup.js
+++ src/ModalPopup.js
@@ -1,21 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import {Button} from 'react-bootstrap';
-import Modal from 'react-bootstrap-modal';
+import { Modal, Button } from 'react-bootstrap';
+// import Modal from 'react-bootstrap-modal';
 
 const ModalPopup = props => {
 
     // console.log(props);
-    if(props.filtered){
-        console.log('full');
-    }else{
-        console.log('empty');
-    }
+    // if(props.filtered){
+    //     console.log('full');
+    // }else{
+    //     console.log('empty');
+    // }
     return ReactDOM.createPortal(
         <div>
-        {/* {console.log(this.props)} */}
-        <Modal.Title>Modal title</Modal.Title>
-        
+            {/* {console.log(this.props)} */}
+            {/* <Modal> */}
+            <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <p>Modal body text goes here.</p>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary">Close</Button>
+                <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
+            {/* </Modal> */}
+
         </div>, document.querySelector("#modal"));
 }
 
